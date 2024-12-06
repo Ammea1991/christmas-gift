2d7577c5b127b80b2bca9d4c4fe43d4f37198073
ameazza <ameazza@it.iliad.com>
2024-12-06 12:29:08 +0100

    feat: mongodb and handling messages

--- app.vue
+++ app.vue
@@ -1,6 +1,17 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+	<div>
+		<RouterView />
+	</div>
 </template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+h1 {
+	font-family: Arial, sans-serif;
+	text-align: center;
+	margin-top: 20vh;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+	<div>
+		<h1>{{ message }}</h1>
+	</div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+const token = route.query.token || null;
+const message = ref("Caricamento...");
+
+onMounted(async () => {
+	if (token) {
+		try {
+			debugger;
+			const response = await fetch(`/api/message/${token}`);
+			debugger;
+			const data = await response.json();
+			if (data.error) {
+				message.value = data.error;
+			} else {
+				message.value = data.message;
+			}
+		} catch (error) {
+			message.value = "Errore nel caricamento del messaggio.";
+		}
+	} else {
+		message.value = "Token non valido.";
+	}
+});
+</script>
